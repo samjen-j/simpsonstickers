11c57235dda99fc7df930e452debd8d6e657feb0
samjen-j <80285130+samjen-j@users.noreply.github.com>
2022-10-14 14:50:13 -0600

    yep

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo chmod -R 777 /home/ec2-user/simpsons-app
+sudo chown -R ec2-user:ec2-user /home/ec2-user/simpsons-app/
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
